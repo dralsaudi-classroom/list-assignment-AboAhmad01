c43cf4be2255425b5052cff8086c097e51f06dac
AboAhmad01 <a.bamukhayyar357@gmail.com>
2024-11-28 13:14:12 +0300

    aq

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -1,26 +1,36 @@
 package com.example.project;
 
 public class ListTester {
-	public static <T > void circularLeftShift ( List <T > list , int n ) {
-		for ( int i = 0; i < n ; i ++) {
-		list.findFirst() ;
-		T elem = list.retrieve();
-		list.remove() ;
-		while (!list.last())
-		list.findNext() ;
-		list.insert(elem) ;
+	public static <T> void circularLeftShift(List<T> list, int n) {
+		
+		for (int i = 0; i < n; i++) {
+			list.findFirst();
+			T tmp = list.retrieve();
+			list.remove();
+			if (!list.empty())
+				while (!list.last())
+					list.findNext();
+			list.insert(tmp);
 		}
-		}
-	public static <T > void reverseCopy ( DLL <T > l1 ,DLL <T > l2 ) {
-			if ( l1.empty() )
-			return;
-			while (!l1.last() )
-			l1.findNext () ;
-			while (!l1.first() ) {
-			l2.insert (l1.retrieve()) ;
-			l1.findPrevious () ;
-			}
-			l2.insert (l1.retrieve()) ;
-			}
 
+	}
+
+	public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2) {
+		if (l1.empty())
+			return;
+
+		// Go to last
+		while (!l1.last())
+			l1.findNext();
+
+		// Copying
+		while (!l1.first()) {
+			l2.insert(l1.retrieve());
+			l1.findPrevious();
+		}
+
+		// Last element
+		l2.insert(l1.retrieve());
+
+	}
 }
